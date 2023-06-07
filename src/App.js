c8557f3782012212e5c1f48a8dd0fb844719fdad
Segun Jeremiah Jimoh <helenayomide81@gmail.com>
2023-06-07 14:13:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Useroutput from './component/Useroutput';
+import { Userinput } from './component/userinput';
+
+function App() {
+  return (
+    <div className="App">
+    <p className="apptwo"></p>
+   <Useroutput/>
+   <Userinput/>
+   <Useroutput/>
+   <Userinput/>
+   <Useroutput/>
+   <Userinput/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Useroutput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Useroutput(){
+  return (
+    <div>
+    <p>my name is ayomide</p>
+    <p>im working on my assessment</p>
+    </div>
+  )
+}
+
+export default Useroutput
+
